0049b942c4c9d21949bbcc66ed30a9ddb3c900f6
andrewg117 <andrewjgrant7@gmail.com>
2021-04-01 17:22:33 -0400

    worked on layout added input section

--- index.js
+++ index.js
@@ -1,62 +1,52 @@
-const defaultText = 'Default text to use in the project. It will, of course, change over time. Default text to use in the project. It will, of course, change over time. Default text to use in the project. It will, of course, change over time. Default text to use in the project. It will, of course, change over time.';
+const defaultText =
+  "Default text to use in the project. It will, of course, change over time. Default text to use in the project. It will, of course, change over time. Default text to use in the project. It will, of course, change over time. Default text to use in the project. It will, of course, change over time.";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    
+
     this.handleClick = this.handleClick.bind(this);
     this.handleChange = this.handleChange.bind(this);
-    
+
     this.state = {
       text: defaultText,
       blockGroup: [],
       currentBlock: 0,
-      displayText: 'Load Text'
+      displayText: "Load Text"
     };
   }
-  
+
   handleChange(e) {
     this.setState({
       text: e.target.value,
       blockGroup: this.convetText().map((text, index) => (
-        <Block 
-          key={index}
-          text={text}
-          />
+        <Block key={index} text={text} />
       )),
       displayText: this.state.blockGroup[this.state.currentBlock].props.text
     });
   }
-  
-  
+
   handleClick(id, value) {
     this.setState({
       displayText: value
     });
   }
-  
-  
+
   convetText() {
-    const arr = this.state.text.split(' ');
+    const arr = this.state.text.split(" ");
     return arr;
   }
-  
+
   componentDidMount() {
     this.setState({
       blockGroup: this.convetText().map((text, index) => (
-        <Block 
-          key={index}
-          text={text}
-          handleClick={this.handleClick}
-          />
+        <Block key={index} text={text} handleClick={this.handleClick} />
       ))
       // displayText: this.state.blockGroup[this.state.currentBlock].props.text
     });
   }
-  
+
   render() {
-    
-    
     return (
       <div id="main-container">
         <textarea
@@ -68,8 +58,27 @@ class App extends React.Component {
         <section id="block-view">
           <h1>{this.state.displayText}</h1>
         </section>
+        <section id="input-view">
+          <button className="btn btn-light">Start</button>
+          <button className="btn btn-light">Reset</button>
+          
+          {/*  <div className="dropdown">
+            <button
+              className="btn btn-light dropdown-toggle"
+              type="button"
+              data-toggle="dropdown">
+              WPM
+            </button>
+            <ul className="dropdown-menu">
+               <a className="dropdown-item" href="#">HTML</a>
+               <a className="dropdown-item" href="#">HTML</a>
+               <a className="dropdown-item" href="#">HTML</a>
+               <a className="dropdown-item" href="#">HTML</a>
+            </ul>
+          </div> */}
+        </section>
       </div>
-      )
+    );
   }
 }
 
@@ -81,20 +90,26 @@ class Block extends React.Component {
       isSelected: false
     };
   }
-  
+
   // will change to selecting only one at a time
   handleClick(e) {
     this.props.handleClick(this.props.key, this.props.text);
-    
+
     this.setState({
       isSelected: true
     });
   }
-  
+
   render() {
     return (
-        <div id="textBlock" class={this.state.isSelected ? "isActive" : ""} onClick={this.handleClick}>{this.props.text}</div>
-      )
+      <div
+        id="textBlock"
+        className={this.state.isSelected ? "isActive" : ""}
+        onClick={this.handleClick}
+      >
+        {this.props.text}
+      </div>
+    );
   }
 }
 
